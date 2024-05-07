74559f6e2dfcce5ca2a9e8accc3fedcfe9802690
Darklord Inc <avorobev341@gmail.com>
2024-05-07 10:41:47 +0400

    Удалил лишний импорт

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {
   AddedTasksComponent,
-  ModalTaskComponent,
   SelectedTaskComponent,
   ToDoListComponent,
   ToDoListFormComponent
